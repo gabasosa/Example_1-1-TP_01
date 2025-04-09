af5cc83a7b3564281d38d2e75379c679115b1a45
Gabriel Sosa <isosa@fi.uba.ar>
2025-04-09 20:48:31 +0000

    Ejercicio terminado.

--- main.cpp
+++ main.cpp
@@ -3,21 +3,34 @@
 
 int main()
 {
-    DigitalIn gasDetector(D2);
+    // Agrego un LED más.
+    DigitalIn gasDetector(D2), gasDetector2(D3);
 
-    DigitalOut alarmLed(LED1);
+    DigitalOut alarmLed(LED1), alarmLed2(LED2);
 
-    gasDetector.mode(PullDown);
+    gasDetector.mode(PullDown), gasDetector2.mode(PullDown);
 
-    alarmLed = OFF;
+    alarmLed = OFF, alarmLed2 = OFF;
 
+    // Imprimir "Hello World" por puerto serie (9600 baudios por defecto).
+    printf("%s\n", "Hello World");
+    
     while (true) {
+        
         if ( gasDetector == ON ) {
             alarmLed = ON;
         }
         
         if ( gasDetector == OFF ) {
             alarmLed = OFF;
+        }
+
+        if ( gasDetector2 == ON ) {
+            alarmLed2 = ON;
+        }
+        
+        if ( gasDetector2 == OFF ) {
+            alarmLed2 = OFF;
         }
     }
 }
